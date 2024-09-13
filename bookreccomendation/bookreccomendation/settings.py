9bb66fcfc546cba77e92c313642e141c531d1a2b
Viraj-005 <124769627+Viraj-005@users.noreply.github.com>
2024-09-13 22:25:46 +0530

    commit

--- bookreccomendation/bookreccomendation/settings.py
+++ bookreccomendation/bookreccomendation/settings.py
@@ -26,9 +26,9 @@ load_dotenv()
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = os.getenv("DEBUG", "True") == "True"
 
-ALLOWED_HOSTS = ['127.0.0.1', '']
+ALLOWED_HOSTS = os.getenv("DJANGO_ALLOWED_HOSTS", "127.0.0.1,localhost").split(",")
 
 
 # Application definition
